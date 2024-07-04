6e3c9837856451048cc72cb3855be15ce1ec78aa
rybaz <>
2024-07-04 11:50:53 -0400

    removed opengl.driSupport

--- machines/morgoth/default.nix
+++ machines/morgoth/default.nix
@@ -15,7 +15,6 @@
 
   hardware = {
       opengl.enable = true;
-      opengl.driSupport = true;
       opengl.driSupport32Bit = true;
       opengl.extraPackages = with pkgs; [
           intel-media-driver
